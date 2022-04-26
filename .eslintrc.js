76501dcc331fc147774ce22ac0bf44430fa4f403
Nurliman Diara <nurlimandiara@gmail.com>
2022-04-26 12:00:57 +0700

    setup eslint

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    amd: true,
+    node: true,
+  },
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "solid"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:solid/typescript",
+  ],
+};
