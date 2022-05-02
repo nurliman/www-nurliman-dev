fb3fc19f810d183be805b589992e740f3d53e1e0
nurliman <nurlimandiara@gmail.com>
2022-05-02 21:23:58 +0700

    using nextjs lint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,13 +6,11 @@ module.exports = {
     node: true,
   },
   parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint", "solid"],
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:solid/typescript",
-  ],
+  plugins: ["@typescript-eslint"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "next/core-web-vitals"],
   rules: {
     "@typescript-eslint/no-var-requires": 0,
+    "@next/next/no-img-element": 0,
+    "react-hooks/exhaustive-deps": 0,
   },
 };
