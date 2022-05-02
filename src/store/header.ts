c3dc1a1a561c9ed96383d1175dd3c8663d607785
nurliman <nurlimandiara@gmail.com>
2022-05-02 21:26:55 +0700

    remove nanostores

--- src/store/header.ts
+++
@@ -1,9 +0,0 @@
-import { atom } from "nanostores";
-
-const initialValue = { show: false };
-
-export const headerStore = atom(initialValue);
-
-export function setShow(show: boolean) {
-  headerStore.set({ ...headerStore.get(), show });
-}
--- src/store/me.ts
+++
@@ -1,8 +0,0 @@
-import { atom } from "nanostores";
-
-const initialValue = {
-  name: "Nurliman Diara",
-  titles: ["Web Developer", "Frontend Developer"],
-};
-
-export const meStore = atom(initialValue);
