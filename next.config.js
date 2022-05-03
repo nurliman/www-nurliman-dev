451204ec77d43949ca87ab3ad4a0417a8c2ca3ac
nurliman <nurlimandiara@gmail.com>
2022-05-03 18:11:22 +0700

    add `next-build-id`

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 const withPreact = require("next-plugin-preact");
+const nextBuildId = require("next-build-id");
 
 /** @type {import('next').NextConfig} */
-const config = {};
+const config = {
+  generateBuildId: () => nextBuildId({ dir: __dirname }),
+};
 
 module.exports = withPreact(config);
