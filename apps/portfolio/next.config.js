55abe4d28239bed23ea3ca23be7b090317a2075a
nurliman <nurlimandiara@gmail.com>
2022-05-05 11:58:45 +0700

    optimize className

--- apps/portfolio/next.config.js
+++ apps/portfolio/next.config.js
@@ -1,9 +1,11 @@
-const withPreact = require("next-plugin-preact");
+const withPlugins = require("next-compose-plugins");
 const nextBuildId = require("next-build-id");
+const nextPreact = require("next-plugin-preact");
+const nextOptimizedClassnames = require("next-optimized-classnames");
 
 /** @type {import('next').NextConfig} */
 const config = {
   generateBuildId: () => nextBuildId({ dir: __dirname }),
 };
 
-module.exports = withPreact(config);
+module.exports = module.exports = withPlugins([nextPreact, nextOptimizedClassnames], config);
