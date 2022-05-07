80bc4cb6c06531b0e7dc4a80388e1d364f2977d9
nurliman <nurlimandiara@gmail.com>
2022-05-07 23:44:03 +0700

    recreate styling of resume page

--- apps/portfolio/src/components/Sections/Resume/Resume.tsx
+++ apps/portfolio/src/components/Sections/Resume/Resume.tsx
@@ -1,268 +1,210 @@
 import Section from "components/Section";
 import PageTitle from "components/PageTitle";
-// import styles from "./Resume.module.scss";
+import BlockTitle from "components/BlockTitle";
+import Timeline from "components/Timeline";
+import ProgressBar from "components/ProgressBar";
+import styles from "./Resume.module.scss";
 
 export default function ResumeSection() {
   return (
     <Section sectionId="resume">
       <PageTitle>Resume</PageTitle>
 
-      <div className="row">
-        <div className="col-xs-12 col-sm-7">
-          <div className="block-title">
-            <h3>Education</h3>
+      <div className={styles.resumeGrid}>
+        <div className={styles.resumeGridColLeft}>
+          <div className={styles.resumeGridCell}>
+            <BlockTitle>Education</BlockTitle>
+
+            <Timeline>
+              <Timeline.Item
+                period="2008"
+                company="University of Studies"
+                title="Frontend Development"
+                text={
+                  <>
+                    Maecenas finibus nec sem ut imperdiet. Ut tincidunt est ac dolor aliquam
+                    sodales. Phasellus sed mauris hendrerit, laoreet sem in, lobortis ante.
+                  </>
+                }
+              />
+              <Timeline.Item
+                period="2007"
+                company="University of Studies"
+                title="Graphic Design"
+                text={
+                  <>
+                    Aliquam tincidunt malesuada tortor vitae iaculis. In eu turpis iaculis, feugiat
+                    risus quis, aliquet urna. Quisque fringilla mollis risus, eu pulvinar dolor.
+                  </>
+                }
+              />
+            </Timeline>
           </div>
 
-          <div className="timeline timeline-second-style clearfix">
-            <div className="timeline-item clearfix">
-              <div className="left-part">
-                <h5 className="item-period">2008</h5>
-                <span className="item-company">University of Studies</span>
-              </div>
-              <div className="divider"></div>
-              <div className="right-part">
-                <h4 className="item-title">Frontend Development</h4>
-                <p>
-                  Maecenas finibus nec sem ut imperdiet. Ut tincidunt est ac dolor aliquam sodales.
-                  Phasellus sed mauris hendrerit, laoreet sem in, lobortis ante.
-                </p>
-              </div>
-            </div>
+          <div className={styles.resumeGridCell}>
+            <BlockTitle>Experience</BlockTitle>
 
-            <div className="timeline-item clearfix">
-              <div className="left-part">
-                <h5 className="item-period">2007</h5>
-                <span className="item-company">University of Studies</span>
-              </div>
-              <div className="divider"></div>
-              <div className="right-part">
-                <h4 className="item-title">Graphic Design</h4>
-                <p>
-                  Aliquam tincidunt malesuada tortor vitae iaculis. In eu turpis iaculis, feugiat
-                  risus quis, aliquet urna. Quisque fringilla mollis risus, eu pulvinar dolor.
-                </p>
-              </div>
-            </div>
-          </div>
-
-          <div className="white-space-50"></div>
-
-          <div className="block-title">
-            <h3>Experience</h3>
-          </div>
-
-          <div className="timeline timeline-second-style clearfix">
-            <div className="timeline-item clearfix">
-              <div className="left-part">
-                <h5 className="item-period">2016 - Current</h5>
-                <span className="item-company">Google</span>
-              </div>
-              <div className="divider"></div>
-              <div className="right-part">
-                <h4 className="item-title">Lead Ui/Ux Designer</h4>
-                <p>
-                  Praesent dignissim sollicitudin justo, sed elementum quam lacinia quis. Phasellus
-                  eleifend tristique posuere. Sed vitae dui nec magna.
-                </p>
-              </div>
-            </div>
-
-            <div className="timeline-item clearfix">
-              <div className="left-part">
-                <h5 className="item-period">2013 - 2016</h5>
-                <span className="item-company">Adobe</span>
-              </div>
-              <div className="divider"></div>
-              <div className="right-part">
-                <h4 className="item-title">Senior Ui/Ux Designer</h4>
-                <p>
-                  Maecenas tempus faucibus rutrum. Duis eu aliquam urna. Proin vitae nulla
-                  tristique, ornare felis id, congue libero. Nam volutpat euismod quam.
-                </p>
-              </div>
-            </div>
-
-            <div className="timeline-item clearfix">
-              <div className="left-part">
-                <h5 className="item-period">2011 - 2013</h5>
-                <span className="item-company">Google</span>
-              </div>
-              <div className="divider"></div>
-              <div className="right-part">
-                <h4 className="item-title">Junior Ui/Ux Designer</h4>
-                <p>
-                  Duis mollis nunc quis quam viverra venenatis. Nulla nulla arcu, congue vitae nunc
-                  ac, sodales ultricies diam. Nullam justo leo, tincidunt sit amet.
-                </p>
-              </div>
-            </div>
+            <Timeline>
+              <Timeline.Item
+                period="2016 - Current"
+                company="Google"
+                title="Lead Ui/Ux Designer"
+                text={
+                  <>
+                    Praesent dignissim sollicitudin justo, sed elementum quam lacinia quis.
+                    Phasellus eleifend tristique posuere. Sed vitae dui nec magna.
+                  </>
+                }
+              />
+              <Timeline.Item
+                period="2013 - 2016"
+                company="Adobe"
+                title="Senior Ui/Ux Designer"
+                text={
+                  <>
+                    Maecenas tempus faucibus rutrum. Duis eu aliquam urna. Proin vitae nulla
+                    tristique, ornare felis id, congue libero. Nam volutpat euismod quam.
+                  </>
+                }
+              />
+              <Timeline.Item
+                period="2011 - 2013"
+                company="Google"
+                title="Junior Ui/Ux Designer"
+                text={
+                  <>
+                    Duis mollis nunc quis quam viverra venenatis. Nulla nulla arcu, congue vitae
+                    nunc ac, sodales ultricies diam. Nullam justo leo, tincidunt sit amet.
+                  </>
+                }
+              />
+            </Timeline>
           </div>
         </div>
 
-        <div className="col-xs-12 col-sm-5">
-          <div className="block-title">
-            <h3>
+        <div className={styles.resumeGridColRight}>
+          <div className={styles.resumeGridCell}>
+            <BlockTitle>
               Design <span>Skills</span>
-            </h3>
-          </div>
+            </BlockTitle>
 
-          <div className="skills-info skills-second-style">
-            <div className="skill clearfix">
-              <h4>Web Design</h4>
-              <div className="skill-value">95%</div>
-            </div>
-            <div className="skill-container skill-1">
-              <div className="skill-percentage"></div>
-            </div>
+            <div className={styles.skills}>
+              <div className={styles.skillsItemTop}>
+                <h4>Web Design</h4>
+                <div className={styles.skillsItemValue}>95%</div>
+              </div>
+              <ProgressBar progress={0.95} />
 
-            <div className="skill clearfix">
-              <h4>Print Design</h4>
-              <div className="skill-value">65%</div>
-            </div>
-            <div className="skill-container skill-2">
-              <div className="skill-percentage"></div>
-            </div>
+              <div className={styles.skillsItemTop}>
+                <h4>Print Design</h4>
+                <div className={styles.skillsItemValue}>65%</div>
+              </div>
+              <ProgressBar progress={0.65} />
 
-            <div className="skill clearfix">
-              <h4>Logo Design</h4>
-              <div className="skill-value">80%</div>
-            </div>
-            <div className="skill-container skill-3">
-              <div className="skill-percentage"></div>
-            </div>
+              <div className={styles.skillsItemTop}>
+                <h4>Logo Design</h4>
+                <div className={styles.skillsItemValue}>80%</div>
+              </div>
+              <ProgressBar progress={0.8} />
 
-            <div className="skill clearfix">
-              <h4>Graphic Design</h4>
-              <div className="skill-value">90%</div>
-            </div>
-            <div className="skill-container skill-4">
-              <div className="skill-percentage"></div>
+              <div className={styles.skillsItemTop}>
+                <h4>Graphic Design</h4>
+                <div className={styles.skillsItemValue}>90%</div>
+              </div>
+              <ProgressBar progress={0.9} />
             </div>
           </div>
 
-          <div className="white-space-10"></div>
-
-          <div className="block-title">
-            <h3>
+          <div className={styles.resumeGridCell}>
+            <BlockTitle>
               Coding <span>Skills</span>
-            </h3>
-          </div>
+            </BlockTitle>
 
-          <div className="skills-info skills-second-style">
-            <div className="skill clearfix">
-              <h4>JavaScript</h4>
-              <div className="skill-value">95%</div>
-            </div>
-            <div className="skill-container skill-5">
-              <div className="skill-percentage"></div>
-            </div>
+            <div className={styles.skills}>
+              <div className={styles.skillsItemTop}>
+                <h4>JavaScript</h4>
+                <div className={styles.skillsItemValue}>95%</div>
+              </div>
+              <ProgressBar progress={0.95} />
 
-            <div className="skill clearfix">
-              <h4>PHP</h4>
-              <div className="skill-value">85%</div>
-            </div>
-            <div className="skill-container skill-6">
-              <div className="skill-percentage"></div>
-            </div>
+              <div className={styles.skillsItemTop}>
+                <h4>PHP</h4>
+                <div className={styles.skillsItemValue}>85%</div>
+              </div>
+              <ProgressBar progress={0.85} />
 
-            <div className="skill clearfix">
-              <h4>HTML/CSS</h4>
-              <div className="skill-value">100%</div>
-            </div>
-            <div className="skill-container skill-7">
-              <div className="skill-percentage"></div>
-            </div>
+              <div className={styles.skillsItemTop}>
+                <h4>HTML/CSS</h4>
+                <div className={styles.skillsItemValue}>100%</div>
+              </div>
+              <ProgressBar progress={1} />
 
-            <div className="skill clearfix">
-              <h4>Smarty/Twig</h4>
-              <div className="skill-value">75%</div>
-            </div>
-            <div className="skill-container skill-8">
-              <div className="skill-percentage"></div>
-            </div>
+              <div className={styles.skillsItemTop}>
+                <h4>Smarty/Twig</h4>
+                <div className={styles.skillsItemValue}>75%</div>
+              </div>
+              <ProgressBar progress={0.75} />
 
-            <div className="skill clearfix">
-              <h4>Perl</h4>
-              <div className="skill-value">90%</div>
-            </div>
-            <div className="skill-container skill-9">
-              <div className="skill-percentage"></div>
+              <div className={styles.skillsItemTop}>
+                <h4>Perl</h4>
+                <div className={styles.skillsItemValue}>90%</div>
+              </div>
+              <ProgressBar progress={0.9} />
             </div>
           </div>
 
-          <div className="white-space-10"></div>
+          <div className={styles.resumeGridCell}>
+            <BlockTitle>Knowledges</BlockTitle>
 
-          <div className="block-title">
-            <h3>Knowledges</h3>
-          </div>
-
-          <ul className="knowledges">
-            <li>Marketing</li>
-            <li>Print</li>
-            <li>Digital Design</li>
-            <li>Social Media</li>
-            <li>Time Management</li>
-            <li>Communication</li>
-            <li>Problem-Solving</li>
-            <li>Social Networking</li>
-            <li>Flexibility</li>
-          </ul>
-        </div>
-      </div>
-
-      <div className="white-space-50"></div>
-
-      <div className="row">
-        <div className="col-xs-12 col-sm-12">
-          <div className="block-title">
-            <h3>Certificates</h3>
+            <ul className={styles.knowledges}>
+              <li>Marketing</li>
+              <li>Print</li>
+              <li>Digital Design</li>
+              <li>Social Media</li>
+              <li>Time Management</li>
+              <li>Communication</li>
+              <li>Problem-Solving</li>
+              <li>Social Networking</li>
+              <li>Flexibility</li>
+            </ul>
           </div>
         </div>
       </div>
 
-      <div className="row">
-        <div className="col-xs-12 col-sm-6">
-          <div className="certificate-item clearfix">
-            <div className="certi-logo">
-              <img loading="lazy" src="img/clients/client-1.png" alt="logo" />
-            </div>
+      <BlockTitle>Certificates</BlockTitle>
 
-            <div className="certi-content">
-              <div className="certi-title">
-                <h4>Psyhology of Intertnation Design</h4>
-              </div>
-              <div className="certi-id">
-                <span>Membership ID: XXXX</span>
-              </div>
-              <div className="certi-date">
-                <span>19 April 2018</span>
-              </div>
-              <div className="certi-company">
-                <span></span>
-              </div>
+      <div className={styles.certificates}>
+        <div className={styles.certificate}>
+          <div className={styles.certificateLogo}>
+            <img loading="lazy" src="img/clients/client-1.png" alt="logo" />
+          </div>
+
+          <div className={styles.certificateContent}>
+            <div className={styles.certificateTitle}>
+              <h4>Psyhology of Intertnation Design</h4>
+            </div>
+            <div className={styles.certificateId}>
+              <span>Membership ID: XXXX</span>
+            </div>
+            <div className={styles.certificateDate}>
+              <span>19 April 2018</span>
             </div>
           </div>
         </div>
+        <div className={styles.certificate}>
+          <div className={styles.certificateLogo}>
+            <img loading="lazy" src="img/clients/client-1.png" alt="logo" />
+          </div>
 
-        <div className="col-xs-12 col-sm-6">
-          <div className="certificate-item clearfix">
-            <div className="certi-logo">
-              <img loading="lazy" src="img/clients/client-1.png" alt="logo" />
+          <div className={styles.certificateContent}>
+            <div className={styles.certificateTitle}>
+              <h4>Psyhology of Intertnation Design</h4>
             </div>
-
-            <div className="certi-content">
-              <div className="certi-title">
-                <h4>Psyhology of Intertnation Design</h4>
-              </div>
-              <div className="certi-id">
-                <span>Membership ID: XXXX</span>
-              </div>
-              <div className="certi-date">
-                <span>19 April 2018</span>
-              </div>
-              <div className="certi-company">
-                <span></span>
-              </div>
+            <div className={styles.certificateId}>
+              <span>Membership ID: XXXX</span>
+            </div>
+            <div className={styles.certificateDate}>
+              <span>19 April 2018</span>
             </div>
           </div>
         </div>
