0c00284624c29adbbb5ef280314b6939a5208a30
nurliman <nurlimandiara@gmail.com>
2022-05-09 02:42:52 +0700

    refactor PerfectScrollbar

--- apps/portfolio/src/components/Section/Section.tsx
+++ apps/portfolio/src/components/Section/Section.tsx
@@ -1,6 +1,6 @@
-import React, { ComponentProps, useCallback, useEffect, useRef } from "react";
+import React, { ComponentProps } from "react";
 import clsx from "clsx";
-import PerfectScrollbar from "perfect-scrollbar";
+import { usePerfectScrollbar } from "hooks/usePerfectScrollbar";
 import styles from "./Section.module.scss";
 
 type Props = ComponentProps<"section"> & {
@@ -9,43 +9,11 @@ type Props = ComponentProps<"section"> & {
 };
 
 const Section: React.FC<Props> = ({ className, innerClassName, children, sectionId }) => {
-  const container = useRef<HTMLDivElement>();
-  const perfectScrollbar = useRef<PerfectScrollbar>();
-
-  const psInit = useCallback(() => {
-    perfectScrollbar.current = new PerfectScrollbar(container.current);
-    return perfectScrollbar.current;
-  }, [perfectScrollbar, container]);
-
-  const psDestroy = useCallback(
-    (ps = perfectScrollbar.current) => {
-      typeof ps?.destroy === "function" && ps.destroy();
-      ps = null;
-    },
-    [perfectScrollbar],
-  );
-
-  useEffect(() => {
-    psInit();
-
-    return psDestroy;
-  }, []);
-
-  useEffect(() => {
-    const ps = psInit();
-    const psUpdate = () => typeof ps?.update === "function" && ps.update();
-
-    window.addEventListener("resize", psUpdate);
-
-    return () => {
-      psDestroy(ps);
-      window.removeEventListener("resize", psUpdate);
-    };
-  }, []);
+  const [psRef] = usePerfectScrollbar();
 
   return (
     <section
-      ref={container}
+      ref={psRef}
       data-id={sectionId}
       className={clsx(styles.section, styles.active, className)}
     >
