662289fd1ba199bd217a93bd0a13eadcc930c961
nurliman <nurlimandiara@gmail.com>
2022-05-09 03:38:24 +0700

    remove font `M PLUS 1 Code`

--- apps/portfolio/src/pages/_document.tsx
+++ apps/portfolio/src/pages/_document.tsx
@@ -8,7 +8,7 @@ export default class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link
-            href="https://fonts.googleapis.com/css2?family=M+PLUS+1+Code&family=Poppins:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,200;1,300;1,400;1,500;1,600;1,700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,200;1,300;1,400;1,500;1,600;1,700&display=swap"
             rel="stylesheet"
           />
         </Head>
