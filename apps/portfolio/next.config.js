592648f578388a1c40d5a00e86e8fde5c134bf2f
nurliman <nurlimandiara@gmail.com>
2022-05-09 10:49:32 +0700

    setup `@next/bundle-analyzer`

--- apps/portfolio/next.config.js
+++ apps/portfolio/next.config.js
@@ -2,10 +2,18 @@ const withPlugins = require("next-compose-plugins");
 const nextBuildId = require("next-build-id");
 const nextPreact = require("next-plugin-preact");
 const nextOptimizedClassnames = require("next-optimized-classnames");
+const nextBundleAnalyzer = require("@next/bundle-analyzer");
 
 /** @type {import('next').NextConfig} */
 const config = {
   generateBuildId: () => nextBuildId({ dir: __dirname }),
 };
 
-module.exports = module.exports = withPlugins([nextPreact, nextOptimizedClassnames], config);
+module.exports = module.exports = withPlugins(
+  [
+    nextPreact,
+    nextOptimizedClassnames,
+    [nextBundleAnalyzer({ enabled: process.env.ANALYZE === "true" })],
+  ],
+  config,
+);
