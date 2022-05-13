de1700c6e56dcae637354b828067bc4757e3b68f
nurliman <nurlimandiara@gmail.com>
2022-05-14 00:35:31 +0700

    add manifest.json

--- apps/portfolio/src/pages/_document.tsx
+++ apps/portfolio/src/pages/_document.tsx
@@ -9,6 +9,7 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          <link rel="manifest" href="/manifest.json" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link
