5c5b4f93820799dcb9bb8a4e1495d7677fe24c98
nurliman <nurlimandiara@gmail.com>
2022-05-17 18:04:20 +0700

    ignore recaptcha error

--- apps/portfolio/sentry.client.config.js
+++ apps/portfolio/sentry.client.config.js
@@ -5,4 +5,8 @@ const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 Sentry.init({
   dsn: SENTRY_DSN,
   tracesSampleRate: 1.0,
+  beforeSend(event, hint) {
+    if (hint.originalException === "Timeout") return null;
+    return event;
+  },
 });
