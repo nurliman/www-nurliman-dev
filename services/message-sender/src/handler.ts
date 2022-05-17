d9c393f85697898c350496654248546dda88b3a3
nurliman <nurlimandiara@gmail.com>
2022-05-18 01:23:13 +0700

    add `ENVIRONMENT` to `Env` type

--- services/message-sender/src/handler.ts
+++ services/message-sender/src/handler.ts
@@ -1,4 +1,6 @@
-type Env = Record<string, never>;
+type Env = {
+  ENVIRONMENT: "production" | "staging";
+};
 
 export const handleRequest: ExportedHandlerFetchHandler<Env> = async (request) => {
   return new Response(`request method: ${request.method}`);
