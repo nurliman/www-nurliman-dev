0d081ee95cb47ec7f5ba393f8267774b3c337e64
nurliman <nurlimandiara@gmail.com>
2022-05-18 19:51:10 +0700

    add try...catch

--- services/message-sender/src/handlers/v0/send.ts
+++ services/message-sender/src/handlers/v0/send.ts
@@ -3,8 +3,24 @@ import { Handler } from "@/types";
 const send: Handler = async (request, env) => {
   const url = new URL(request.url);
 
-  if (!env.SENDGRID_API_KEY) {
-    console.error("Error: Please provide env.SENDGRID_API_KEY");
+  try {
+    if (!env.SENDGRID_API_KEY) {
+      console.error("Error: Please provide env.SENDGRID_API_KEY");
+
+      const body = JSON.stringify({
+        timestamp: new Date(),
+        status: 500,
+        error: "Internal Server Error",
+        message: "No message available",
+        path: url.pathname,
+      });
+
+      return new Response(body, { status: 500 });
+    }
+
+    return new Response(JSON.stringify({ name: "send", version: "0.0.1" }));
+  } catch (error) {
+    console.error(error);
 
     const body = JSON.stringify({
       timestamp: new Date(),
@@ -16,8 +32,6 @@ const send: Handler = async (request, env) => {
 
     return new Response(body, { status: 500 });
   }
-
-  return new Response(JSON.stringify({ name: "send", version: "0.0.1" }));
 };
 
 export default send;
