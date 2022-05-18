9d4583def9bdbc39277c381ecfccbc4f6eff13d6
nurliman <nurlimandiara@gmail.com>
2022-05-18 19:49:36 +0700

    add env checking

--- services/message-sender/src/handlers/v0/send.ts
+++ services/message-sender/src/handlers/v0/send.ts
@@ -1,6 +1,22 @@
 import { Handler } from "@/types";
 
-const send: Handler = async () => {
+const send: Handler = async (request, env) => {
+  const url = new URL(request.url);
+
+  if (!env.SENDGRID_API_KEY) {
+    console.error("Error: Please provide env.SENDGRID_API_KEY");
+
+    const body = JSON.stringify({
+      timestamp: new Date(),
+      status: 500,
+      error: "Internal Server Error",
+      message: "No message available",
+      path: url.pathname,
+    });
+
+    return new Response(body, { status: 500 });
+  }
+
   return new Response(JSON.stringify({ name: "send", version: "0.0.1" }));
 };
 
