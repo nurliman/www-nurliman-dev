c5690a598d7938887ed7b15430f34a50c8e752ba
nurliman <nurlimandiara@gmail.com>
2022-05-23 00:15:36 +0700

    remove tsup.config

--- services/message-sender/tsup.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "tsup";
-
-export default defineConfig({
-  entry: { worker: "src/index.ts" },
-  format: ["esm"],
-  bundle: true,
-  sourcemap: true,
-  clean: true,
-});
