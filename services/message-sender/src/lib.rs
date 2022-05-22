3ceaa9d889ee037d90b05eafe1aafd4aee26209a
nurliman <nurlimandiara@gmail.com>
2022-05-23 02:06:15 +0700

    migrate `message-sender` to rust

---
+++ services/message-sender/src/lib.rs
@@ -0,0 +1,45 @@
+use serde_json::json;
+use worker::*;
+
+mod utils;
+
+fn log_request(req: &Request) {
+    console_log!(
+        "{} - [{}], located at: {:?}, within: {}",
+        Date::now().to_string(),
+        req.path(),
+        req.cf().coordinates().unwrap_or_default(),
+        req.cf().region().unwrap_or("unknown region".into())
+    );
+}
+
+#[event(fetch)]
+pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Response> {
+    log_request(&req);
+
+    utils::set_panic_hook();
+
+    let router = Router::new();
+
+    router
+        .get("/", |_, _| Response::ok("Hello from Workers!"))
+        .get("/v0/send", |_, ctx| {
+            let sg_api_key = match ctx.secret("SENDGRID_API_KEY") {
+                Ok(value) => value.to_string(),
+                Err(_) => String::new(),
+            };
+
+            console_log!("Error: Please provide env.SENDGRID_API_KEY");
+
+            if sg_api_key.is_empty() {
+                return Response::error("Some environment variables is not provided.", 500);
+            }
+            Response::from_json(&json!({ "name": "send", "version": "0.0.1" }))
+        })
+        .get("/worker-version", |_, ctx| {
+            let version = ctx.var("WORKERS_RS_VERSION")?.to_string();
+            Response::ok(version)
+        })
+        .run(req, env)
+        .await
+}
--- services/message-sender/src/types.ts
+++
@@ -1,6 +0,0 @@
-export type Env = {
-  ENVIRONMENT: "production" | "staging";
-  SENDGRID_API_KEY?: string;
-};
-
-export type Handler = ExportedHandlerFetchHandler<Env>;
