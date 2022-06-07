c70d4df7cb0dc0c2a3fb2c46fde33431c05cc73c
nurliman <nurlimandiara@gmail.com>
2022-06-07 20:07:16 +0700

    message-sender v1.0.0

--- services/message-sender/api/index.go
+++ services/message-sender/api/index.go
@@ -5,7 +5,7 @@ import (
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	payload := `{"name":"message-sender","version":"0.3.0","status":"online"}`
+	payload := `{"name":"message-sender","version":"1.0.0","status":"online"}`
 
 	w.Header().Set("Content-Type", "application/json")
 	w.Write([]byte(payload))
