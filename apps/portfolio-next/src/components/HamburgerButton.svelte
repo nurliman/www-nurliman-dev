a1d91ca4fd88af6b7792fdfa2c8098d7c6cc3f44
nurliman <nurlimandiara@gmail.com>
2023-11-19 23:59:52 +0700

    Refactor HamburgerButton component to toggle menu icon visibility

--- apps/portfolio-next/src/components/HamburgerButton.svelte
+++ apps/portfolio-next/src/components/HamburgerButton.svelte
@@ -2,7 +2,21 @@
   export let isOpen: boolean;
 </script>
 
-<button class="flex h-auto min-h-[3rem] w-12 items-center justify-center border-r" on:click>
-  <img src="/assets/menu-close.svg" alt="Close" height="24px" width="24px" class:hidden={!isOpen} />
-  <img src="/assets/menu-burger.svg" alt="Menu" height="24px" width="24px" class:hidden={isOpen} />
+<button class="relative h-auto min-h-[3rem] w-12 border-r" on:click>
+  <img
+    src="/assets/menu-burger.svg"
+    alt="Menu"
+    height="24px"
+    width="24px"
+    class="absolute-center opacity-1 transition-opacity"
+    class:!opacity-0={isOpen}
+  />
+  <img
+    src="/assets/menu-close.svg"
+    alt="Close"
+    height="24px"
+    width="24px"
+    class="absolute-center opacity-0 transition-opacity"
+    class:!opacity-100={isOpen}
+  />
 </button>
