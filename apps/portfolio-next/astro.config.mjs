1d05f676052eb4bbe3804e03f99b6bd4e2691734
nurliman <nurlimandiara@gmail.com>
2023-11-20 00:17:45 +0700

    Add unplugin-tailwindcss-mangle plugin to vite config

--- apps/portfolio-next/astro.config.mjs
+++ apps/portfolio-next/astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
+import utwm from "unplugin-tailwindcss-mangle/vite";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind({ applyBaseStyles: false }), svelte()],
+  vite: {
+    plugins: [utwm()],
+  },
 });
