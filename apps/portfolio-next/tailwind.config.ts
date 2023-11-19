df2643651de0e535b30009a4e4c5220057f415d7
nurliman <nurlimandiara@gmail.com>
2023-11-19 20:38:16 +0700

    Add border color customization to tailwind.config.ts

--- apps/portfolio-next/tailwind.config.ts
+++ apps/portfolio-next/tailwind.config.ts
@@ -3,7 +3,11 @@ import type { Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      borderColor: {
+        DEFAULT: "colors.black",
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
