8f3e81ba7ae4109f72e75fe234909900dcf6a962
nurliman <nurlimandiara@gmail.com>
2023-12-03 22:35:12 +0700

    Refactor Footer component

---
+++ apps/portfolio-next/src/components/Footer.vue
@@ -0,0 +1,5 @@
+<template>
+  <footer>
+    This is the footer
+  </footer>
+</template>
--- apps/portfolio-next/src/layouts/default.vue
+++ apps/portfolio-next/src/layouts/default.vue
@@ -1,13 +1,25 @@
 <script setup lang="ts">
+import { ref, watch } from "vue";
 import Header from "~/components/Header.vue";
+import Footer from "~/components/Footer.vue";
+
+const route = useRoute();
+const isHome = ref(route.path === "/");
+
+watch(
+  () => route.path,
+  (path) => {
+    isHome.value = path === "/";
+  },
+);
 </script>
 
 <template>
   <Header />
 
-  <main class="flex h-full pt-[50px] md:pt-14">
+  <main class="flex min-h-full pt-[50px] md:pt-14">
     <slot />
   </main>
 
-  <footer></footer>
+  <Footer v-if="!isHome" />
 </template>
