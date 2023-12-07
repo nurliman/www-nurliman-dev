8aa68c4da1b9a1b4a2eb32158800a0716d178984
nurliman <nurlimandiara@gmail.com>
2023-12-07 20:22:12 +0700

    Remove duplicate utility classes

--- apps/portfolio-next/tailwind.config.js
+++ apps/portfolio-next/tailwind.config.js
@@ -102,17 +102,6 @@ export default {
       });
 
       addUtilities({
-        ".absolute-center": {
-          position: "absolute",
-          top: "50%",
-          left: "50%",
-          transform: "translate(-50%, -50%)",
-        },
-        ".flex-center": {
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-        },
         ".brutal-shadow-sm": {
           boxShadow: "2px 2px 0px rgba(0,0,0,1)",
         },
