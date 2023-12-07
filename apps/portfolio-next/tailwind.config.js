0e966920448c0789a93d1df6c9ae1b9940825a6f
nurliman <nurlimandiara@gmail.com>
2023-12-07 20:53:55 +0700

    move base styles in `tailwind.config` to `main.css`

--- apps/portfolio-next/tailwind.config.js
+++ apps/portfolio-next/tailwind.config.js
@@ -1,5 +1,4 @@
-import twPlugin from "tailwindcss/plugin";
-import nurlimanTailwind from "@nurliman.dev/tailwind";
+import { plugin } from "@nurliman.dev/tailwind";
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -24,52 +23,5 @@ export default {
       },
     },
   },
-  plugins: [
-    nurlimanTailwind(),
-    twPlugin(({ addBase, theme }) => {
-      addBase({
-        ":root,html,body": {
-          minHeight: "100vh",
-          height: "100%",
-          scrollBehavior: "smooth",
-        },
-        body: {
-          padding: "0",
-          margin: "0",
-          fontFamily: `
-            ${theme("fontFamily.transducer")},
-            -apple-system,
-            BlinkMacSystemFont,
-            Segoe UI,
-            Roboto,
-            Oxygen,
-            Ubuntu,
-            Cantarell,
-            Fira Sans,
-            Droid Sans,
-            Helvetica Neue
-          `,
-          WebkitFontSmoothing: "antialiased",
-          MozOsxFontSmoothing: "grayscale",
-        },
-        img: {
-          // no drag
-          "-webkit-user-drag": "none",
-          "-khtml-user-drag": "none",
-          "-moz-user-drag": "none",
-          "-o-user-drag": "none",
-          "-ms-user-drag": "none",
-          "user-drag": "none",
-
-          // no select
-          "-webkit-user-select": "none",
-          "-khtml-user-select": "none",
-          "-moz-user-select": "none",
-          "-o-user-select": "none",
-          "-ms-user-select": "none",
-          "user-select": "none",
-        },
-      });
-    }),
-  ],
+  plugins: [plugin()],
 };
