ed011d9d090989390a682607e4a2f0aa9faaeb99
nurliman <nurlimandiara@gmail.com>
2023-12-14 23:29:46 +0700

    Refactor contact page layout and styles

--- apps/portfolio-next/src/pages/contact.vue
+++ apps/portfolio-next/src/pages/contact.vue
@@ -5,11 +5,12 @@ import TheButton from "~/components/TheButton.vue";
 </script>
 
 <template>
-  <div class="mx-auto flex w-full max-w-[90rem] flex-col">
-    <div class="flex w-full max-w-xl flex-col px-4 sm:px-6 lg:max-w-2xl lg:px-8">
+  <div :class="$style.container">
+    <div :class="$style.innerContainer">
       <div class="mb-6 md:mb-8" />
       <h1 :class="$style.heading1">Get in touch</h1>
       <div class="mb-5" />
+
       <div class="flex flex-col items-start space-y-2">
         <TheButton border color="zinc" shadow="sm" :class="$style.infoBtn"
           >Residence: Bandung, Indonesia</TheButton
@@ -34,17 +35,35 @@ import TheButton from "~/components/TheButton.vue";
           >Email: nurlimandiara@gmail.com</TheButton
         >
       </div>
+
       <div class="mb-5" />
-      <form class="flex flex-col space-y-4">
-        <TheInputText name="name" label="Full Name" placeholder="e.g. John Doe" />
+
+      <form class="z-[2] flex flex-col space-y-4">
         <TheInputText
+          label-class="text-sm md:text-base"
+          input-class="text-sm md:text-base"
+          name="name"
+          label="Full Name"
+          placeholder="e.g. John Doe"
+        />
+        <TheInputText
+          label-class="text-sm md:text-base"
+          input-class="text-sm md:text-base"
           name="email"
           type="email"
           label="Email Address"
           placeholder="e.g. example@email.com"
         />
-        <TheInputText name="subject" label="Subject" placeholder="Subject" />
         <TheInputText
+          label-class="text-sm md:text-base"
+          input-class="text-sm md:text-base"
+          name="subject"
+          label="Subject"
+          placeholder="Subject"
+        />
+        <TheInputText
+          label-class="text-sm md:text-base"
+          input-class="text-sm md:text-base"
           component="textarea"
           name="message"
           label="Message"
@@ -52,21 +71,37 @@ import TheButton from "~/components/TheButton.vue";
           rows="7"
         />
         <div />
-        <TheButton
-          type="submit"
-          color="teal"
-          border
-          shadow
-          class="min-w-[12rem] self-start p-2 md:p-2.5 lg:p-3"
+        <TheButton type="submit" color="teal" border shadow :class="$style.submitBtn"
           >Send</TheButton
         >
       </form>
+
       <div class="mb-16" />
+
+      <img
+        src="/assets/undraw_digital_nomad_re_w8uy.svg"
+        alt="Digital Nomad"
+        loading="lazy"
+        :class="$style.img"
+      />
     </div>
   </div>
 </template>
 
 <style module>
+.container {
+  @apply relative;
+  @apply flex flex-col;
+  @apply mx-auto w-full max-w-[90rem];
+}
+
+.innerContainer {
+  @apply flex flex-col;
+  @apply w-full max-w-xl;
+  @apply mx-auto lg:mx-0;
+  @apply px-4 sm:px-6 lg:px-8;
+}
+
 .heading1 {
   @apply font-transducer-extended font-semibold;
   @apply text-3xl md:text-4xl lg:text-5xl;
@@ -75,6 +110,23 @@ import TheButton from "~/components/TheButton.vue";
 .infoBtn {
   @apply rounded;
   @apply px-2.5 py-px;
-  @apply text-[15px] md:text-base;
+  @apply text-sm md:text-base;
+}
+
+.submitBtn {
+  @apply self-start;
+  @apply p-2 md:p-2.5 lg:p-3;
+  @apply text-sm md:text-base;
+  @apply min-w-[9rem] md:min-w-[12rem];
+}
+
+.img {
+  @apply lg:absolute;
+  @apply lg:top-1/2 lg:-translate-y-1/2;
+  @apply lg:right-3 xl:right-24;
+  @apply w-full max-w-sm sm:max-w-md xl:max-w-lg;
+  @apply min-h-[26.25rem] sm:min-h-[30.625rem];
+  @apply mx-auto lg:mx-0;
+  @apply aspect-[32/35];
 }
 </style>
