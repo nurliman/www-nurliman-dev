f75ca84021c3e752390044c3fab41a20494e4c87
nurliman <nurlimandiara@gmail.com>
2023-12-23 02:59:29 +0700

    Remove "cjs" format

--- packages/env/tsup.config.ts
+++ packages/env/tsup.config.ts
@@ -3,8 +3,7 @@ import { defineConfig } from "tsup";
 export default defineConfig((options) => {
   return {
     entry: ["src/index.ts", "src/core.ts", "src/nuxt.ts"],
-    format: ["cjs", "esm"],
-    target: "es5",
+    format: ["esm"],
     clean: true,
     dts: true,
     sourcemap: !options.watch,
