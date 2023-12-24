545c017d0524637af43ed4d629f3b2507724fbfb
nurliman <nurlimandiara@gmail.com>
2023-12-25 00:46:10 +0700

    Add styles.css to build configuration

--- packages/tailwind/tsup.config.ts
+++ packages/tailwind/tsup.config.ts
@@ -1,11 +1,18 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig((options) => {
-  return {
-    entry: ["src/index.ts"],
-    format: ["cjs", "esm"],
-    clean: true,
-    dts: true,
-    sourcemap: !options.watch,
-  };
+  return [
+    {
+      entry: ["src/index.ts"],
+      format: ["cjs", "esm"],
+      clean: true,
+      dts: true,
+      sourcemap: !options.watch,
+    },
+    {
+      entry: ["src/styles.css"],
+      clean: true,
+      sourcemap: !options.watch,
+    },
+  ];
 });
