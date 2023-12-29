7a2759e2f2e10ea6d2c59c7b2989d66a9689c0cd
nurliman <nurlimandiara@gmail.com>
2023-12-29 20:09:35 +0700

    Update eslint-config/vue.js rules

--- packages/eslint-config/vue.js
+++ packages/eslint-config/vue.js
@@ -19,8 +19,23 @@ module.exports = {
         parser: "@typescript-eslint/parser",
       },
     },
+    {
+      files: [
+        // These pages are not used directly by users so they can have one-word names.
+        "**/pages/**/*.{js,ts,jsx,tsx,vue}",
+        "**/layouts/**/*.{js,ts,jsx,tsx,vue}",
+        "**/app.{js,ts,jsx,tsx,vue}",
+        "**/error.{js,ts,jsx,tsx,vue}",
+        // These files should have multiple words in their names as they are within subdirectories.
+        "**/components/*/**/*.{js,ts,jsx,tsx,vue}",
+      ],
+      rules: { "vue/multi-word-component-names": "off" },
+    },
+    {
+      // Pages and layouts are required to have a single root element if transitions are enabled.
+      files: ["**/pages/**/*.{js,ts,jsx,tsx,vue}", "**/layouts/**/*.{js,ts,jsx,tsx,vue}"],
+      rules: { "vue/no-multiple-template-root": "error" },
+    },
   ],
-  rules: {
-    "vue/multi-word-component-names": "warn",
-  },
+  rules: {},
 };
