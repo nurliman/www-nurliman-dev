7c3526129d775d9a9deb33ce4e3e90c0e3373e21
nurliman <nurlimandiara@gmail.com>
2023-12-29 20:14:35 +0700

    wrap default layout in div

--- apps/portfolio-next/src/layouts/default.vue
+++ apps/portfolio-next/src/layouts/default.vue
@@ -4,19 +4,21 @@ import TheFooter from "~/components/TheFooter.vue";
 </script>
 
 <template>
-  <TheHeader />
+  <div class="contents">
+    <TheHeader />
 
-  <div
-    :class="[
-      'flex min-h-full flex-col pt-[50px] md:pt-14',
-      'bg-white text-black',
-      'dark:bg-black dark:text-white',
-    ]"
-  >
-    <main class="flex flex-1 flex-col">
-      <slot />
-    </main>
+    <div
+      :class="[
+        'flex min-h-full flex-col pt-[50px] md:pt-14',
+        'bg-white text-black',
+        'dark:bg-black dark:text-white',
+      ]"
+    >
+      <main class="flex flex-1 flex-col">
+        <slot />
+      </main>
 
-    <TheFooter v-if="$route.path !== '/'" />
+      <TheFooter v-if="$route.path !== '/'" />
+    </div>
   </div>
 </template>
