89881935699dd5fe227eb2f6a6bcaf140d46e29d
nurliman <nurlimandiara@gmail.com>
2024-01-05 21:27:30 +0700

    Convert ComingSoon.vue to solidjs

---
+++ apps/portfolio-next/src/components/ComingSoon.tsx
@@ -0,0 +1,27 @@
+import { createSignal, onCleanup, onMount } from "solid-js";
+
+export default function ComingSoon() {
+  const [dots, setDots] = createSignal("...");
+
+  onMount(() => {
+    const timer = setInterval(() => {
+      if (dots().length >= 3) {
+        setDots(".");
+      } else {
+        setDots((dots) => dots + ".");
+      }
+    }, 500);
+
+    onCleanup(() => {
+      clearInterval(timer);
+    });
+  });
+
+  return (
+    <div class="flex-center flex-1">
+      <span class="font-transducer-extended text-center text-3xl font-semibold md:text-4xl lg:text-5xl">
+        Coming soon{dots()}
+      </span>
+    </div>
+  );
+}
--- apps/portfolio-next/src/components/pages/BlogPage.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts">
-import ComingSoon from "~/components/ComingSoon.vue";
-</script>
-
-<template>
-  <ComingSoon />
-</template>
