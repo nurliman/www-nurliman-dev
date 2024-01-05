6c720e888f462044be5320061ae55d72219bbdae
nurliman <nurlimandiara@gmail.com>
2024-01-05 21:10:15 +0700

    Add solid-js integration

--- apps/portfolio-next/astro.config.mjs
+++ apps/portfolio-next/astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from "astro/config";
+import solidJs from "@astrojs/solid-js";
 import vue from "@astrojs/vue";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
+    solidJs(),
     vue({
       appEntrypoint: "/src/pages/_app",
       template: {
