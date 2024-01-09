4cc5158882876355a2dbb743e7f7468090d33b6f
nurliman <nurlimandiara@gmail.com>
2024-01-09 17:53:26 +0700

    drop vue

--- apps/portfolio-next/astro.config.mjs
+++ apps/portfolio-next/astro.config.mjs
@@ -1,20 +1,11 @@
 import { defineConfig } from "astro/config";
 import solidJs from "@astrojs/solid-js";
-import vue from "@astrojs/vue";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     solidJs(),
-    vue({
-      appEntrypoint: "/src/pages/_app",
-      template: {
-        transformAssetUrls: {
-          includeAbsolute: false,
-        },
-      },
-    }),
     tailwind({
       applyBaseStyles: false,
     }),
