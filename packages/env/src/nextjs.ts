75c3e42cf46cd38052541cdaaf12b63e88f3fa13
nurliman <nurlimandiara@gmail.com>
2024-01-10 21:47:05 +0700

    Add Next.js support to env package

---
+++ packages/env/src/nextjs.ts
@@ -0,0 +1,67 @@
+import type { AnySchema } from "./utils";
+import type { ServerClientOptions, StrictOptions } from "./core";
+import { createEnv as createEnvCore } from "./core";
+
+const CLIENT_PREFIX = "NEXT_PUBLIC_" as const;
+type ClientPrefix = typeof CLIENT_PREFIX;
+
+type Options<
+  TServer extends Record<string, AnySchema>,
+  TClient extends Record<`${ClientPrefix}${string}`, AnySchema>,
+  TShared extends Record<string, AnySchema>,
+> = Omit<
+  StrictOptions<ClientPrefix, TServer, TClient, TShared> &
+    ServerClientOptions<ClientPrefix, TServer, TClient>,
+  "runtimeEnvStrict" | "runtimeEnv" | "clientPrefix"
+> &
+  (
+    | {
+        /**
+         * Manual destruction of `process.env`. Required for Next.js < 13.4.4.
+         */
+        runtimeEnv: StrictOptions<ClientPrefix, TServer, TClient, TShared>["runtimeEnvStrict"];
+        experimental__runtimeEnv?: never;
+      }
+    | {
+        runtimeEnv?: never;
+        /**
+         * Can be used for Next.js ^13.4.4 since they stopped static analysis of server side `process.env`.
+         * Only client side `process.env` is statically analyzed and needs to be manually destructured.
+         */
+        experimental__runtimeEnv: Record<
+          | {
+              [TKey in keyof TClient]: TKey extends `${ClientPrefix}${string}` ? TKey : never;
+            }[keyof TClient]
+          | {
+              [TKey in keyof TShared]: TKey extends string ? TKey : never;
+            }[keyof TShared],
+          string | boolean | number | undefined
+        >;
+      }
+  );
+
+export function createEnv<
+  TServer extends Record<string, AnySchema> = NonNullable<unknown>,
+  TClient extends Record<`${ClientPrefix}${string}`, AnySchema> = NonNullable<unknown>,
+  TShared extends Record<string, AnySchema> = NonNullable<unknown>,
+>(opts: Options<TServer, TClient, TShared>) {
+  const client = typeof opts.client === "object" ? opts.client : {};
+  const server = typeof opts.server === "object" ? opts.server : {};
+  const shared = opts.shared;
+
+  const runtimeEnv = opts.runtimeEnv
+    ? opts.runtimeEnv
+    : {
+        ...process.env,
+        ...opts.experimental__runtimeEnv,
+      };
+
+  return createEnvCore<ClientPrefix, TServer, TClient, TShared>({
+    ...opts,
+    shared,
+    client,
+    server,
+    clientPrefix: CLIENT_PREFIX,
+    runtimeEnv,
+  });
+}
--- packages/env/tsup.config.ts
+++ packages/env/tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "tsup";
 
 export default defineConfig((options) => {
   return {
-    entry: ["src/index.ts", "src/core.ts", "src/nuxt.ts"],
+    entry: ["src/index.ts", "src/core.ts", "src/nuxt.ts", "src/nextjs.ts"],
     format: ["esm"],
     clean: true,
     dts: true,
