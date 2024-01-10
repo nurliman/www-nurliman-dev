e41be6e3b2d419c11525c1b57c518c3c71e86c37
nurliman <nurlimandiara@gmail.com>
2024-01-10 15:00:25 +0700

    Update experiences and technical skills

--- apps/portfolio-next/src/data/me.ts
+++ apps/portfolio-next/src/data/me.ts
@@ -18,14 +18,61 @@ export const me = {
   },
   experiences: [
     {
-      period: "2020 - Current",
+      period: "Jan 2023 - Jan 2024 (1 year)",
       company: "PT. Knitto Tekstil Indonesia",
-      title: "IT Programmer / Web Developer",
-      description:
-        "Develop and maintain main website, online ordering web app and some internal apps.",
+      title: "Lead Frontend & DevOps Engineer",
+      description: `
+As the Lead Frontend & DevOps Engineer at PT. Knitto Tekstil Indonesia, I spearheaded the integration of cutting-edge technologies to optimize software development processes and enhance overall system efficiency. In this pivotal role, I undertook the following responsibilities:
+<br /><br />
+**Frontend Development:**
+- Led a team of frontend developers in designing and implementing responsive, user-friendly interfaces for web applications, ensuring seamless user experiences.
+- Developed and maintained efficient, reusable, and scalable code using modern JavaScript frameworks such as React.js and Next.js.
+- Collaborated closely with UI/UX designers to translate design mockups into functional frontend components, ensuring alignment with the overall project vision.
+
+**DevOps Integration:**
+- Pioneered the implementation of DevOps best practices to streamline the software development lifecycle, resulting in a significant reduction in deployment time and enhanced collaboration between development and operations teams.
+- Automated deployment processes using tools such as Docker and Kubernetes, leading to a more reliable and scalable infrastructure.
+- Implemented continuous integration and continuous delivery (CI/CD) pipelines, facilitating rapid and reliable software releases.
+
+**Infrastructure Management:**
+- Orchestrated cloud infrastructure deployment and management, leveraging services from providers such as Google Cloud Platform or Vercel, ensuring optimal performance, scalability, and cost-effectiveness.
+- Implemented monitoring and alerting systems to proactively identify and address potential issues, minimizing downtime and improving system reliability.
+
+**Team Leadership:**
+- Provided mentorship and guidance to the frontend and DevOps teams, fostering a collaborative and innovative work environment.
+- Conducted regular code reviews to ensure code quality, adherence to best practices, and knowledge sharing among team members.
+- Collaborated with cross-functional teams, including product managers and backend engineers, to deliver integrated solutions that met both technical and business requirements.
+`,
     },
     {
-      period: "2019 - 2020",
+      period: "Sep 2020 - Jan 2024 (3 years 5 months)",
+      company: "PT. Knitto Tekstil Indonesia",
+      title: "IT Programmer / Web Developer",
+      description: `
+As an IT Programmer/Web Developer at PT. Knitto Tekstil Indonesia, I played a pivotal role in advancing the company's digital presence and internal workflow through the development and maintenance of various applications. Key responsibilities and achievements include:
+<br /><br />
+**Main Website Development:**
+- Spearheaded the design and development of the company's main website, focusing on creating an intuitive and visually appealing online platform.
+- Implemented responsive web design principles to ensure seamless user experiences across various devices, contributing to a 20% increase in website traffic.
+- Collaborated closely with cross-functional teams to gather requirements and translate them into functional features, enhancing the overall online brand presence.
+
+**Online Ordering Web App:**
+- Led the development of a user-friendly online ordering web application, streamlining the customer ordering process.
+- Integrated secure payment gateways and implemented robust authentication mechanisms to ensure the confidentiality and integrity of customer data.
+- Improved the app's performance through optimization techniques, resulting in a 15% decrease in page load times and increased customer satisfaction.
+
+**Internal Apps Development:**
+- Developed and maintained several internal applications to enhance operational efficiency.
+- Implemented custom features based on user feedback, resulting in a 25% reduction in manual data entry and improved overall workflow automation.
+
+**Technological Stack:**
+- Utilized a stack comprising HTML, CSS, JavaScript, and various frontend frameworks to create dynamic and engaging user interfaces.
+- Integrated backend technologies such as Node.js and Express to build robust server-side components for web applications.
+- Pioneered the implementation of version control using Git to manage codebase changes efficiently and facilitate collaboration among the development team.
+`,
+    },
+    {
+      period: "Jul 2019 - Aug 2020 (1 year 2 months)",
       company: "PT. Grasindo Anugrah Pratama",
       title: "IT & Graphic Designer",
       description:
@@ -51,12 +98,12 @@ export const me = {
   technicalSkills: [
     { name: "Kubernetes", value: 0.77 },
     { name: "JavaScript/TypeScript", value: 0.97 },
-    { name: "React", value: 0.94 },
+    { name: "React", value: 0.98 },
     { name: "React Native", value: 0.89 },
     { name: "Next.js", value: 0.96 },
     { name: "Node.js", value: 0.93 },
-    { name: "HTML/CSS", value: 0.88 },
-    { name: "Git/Github", value: 0.85 },
+    { name: "HTML/CSS", value: 0.98 },
+    { name: "Git/Github", value: 0.95 },
     { name: "Go", value: 0.85 },
     { name: "Wordpress", value: 0.9 },
   ],
