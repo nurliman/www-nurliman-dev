91684fe90096780e10e46627ae1cf5f60dd36094
nurliman <nurlimandiara@gmail.com>
2024-01-10 23:04:01 +0700

    Add @sentry/astro integration

--- apps/portfolio/astro.config.mjs
+++ apps/portfolio/astro.config.mjs
@@ -3,9 +3,12 @@ import solidJs from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/static";
 
+import sentry from "@sentry/astro";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [
+    sentry(),
     solidJs(),
     tailwind({
       applyBaseStyles: false,
