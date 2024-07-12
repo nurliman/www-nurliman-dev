2fc0146bec21624ff814b39b2c6c30f28f98231d
Nurliman Diara Aria <54785553+nurliman@users.noreply.github.com>
2024-07-12 16:02:43 +0700

    Update environment configuration

--- apps/message-sender/env.mjs
+++ apps/message-sender/env.mjs
@@ -14,10 +14,5 @@ export const env = createEnv({
     CF_TURNSTILE_SECRET_KEY: vPipe(vString(), vMinLength(1)),
   },
   client: {},
-  runtimeEnv: {
-    RECIPIENT_EMAIL: process.env.RECIPIENT_EMAIL,
-    SENDER_EMAIL: process.env.SENDER_EMAIL,
-    ELASTIC_EMAIL_API_KEY: process.env.ELASTIC_EMAIL_API_KEY,
-    CF_TURNSTILE_SECRET_KEY: process.env.CF_TURNSTILE_SECRET_KEY,
-  },
+  runtimeEnv: import.meta.env,
 });
