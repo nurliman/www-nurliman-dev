b705e154beca4f62bb8cda5052636e6bb46683fe
Nurliman Diara Aria <nurlimandiara@gmail.com>
2024-10-24 20:38:37 +0700

    update `message-sender`

---
+++ apps/message-sender/app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import type { ReactNode } from "react";
+
+export const metadata: Metadata = {
+  title: "Message Sender API",
+  description: "Message Sender API",
+  icons: [
+    { rel: "icon", url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+    { rel: "icon", url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    { rel: "apple-touch-icon", url: "/apple-touch-icon.png" },
+  ],
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
--- apps/message-sender/next.config.mjs
+++ apps/message-sender/next.config.mjs
@@ -1,6 +1,16 @@
 import "./env.mjs";
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  poweredByHeader: false,
+  eslint: {
+    // Temporarily ignore Next.js ESLint errors.
+    // This project uses Biome for linting instead of ESLint.
+    // Re-enable the Next.js linter when Next.js supports custom linters
+    // (in this case, Biome).
+    ignoreDuringBuilds: true,
+  },
+};
 
 export default nextConfig;
