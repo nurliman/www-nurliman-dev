cd89e2ccd86f17f7af81e1112d3843259280d5ac
Nurliman Diara Aria <nurlimandiara@gmail.com>
2024-10-24 20:52:47 +0700

    update `emails` package

--- packages/emails/src/index.ts
+++ packages/emails/src/index.ts
@@ -2,6 +2,10 @@ import type { ContactFormEmailProps } from "@/templates/contact-form.ts";
 import { contactFormHtml } from "@generated/index.ts";
 import pupa from "pupa";
 
-export function renderContactFormHtml(props: ContactFormEmailProps) {
-  return pupa(contactFormHtml, props);
-}
+/**
+ * Renders the contact form email template
+ * @param {ContactFormEmailProps} props - The properties for the contact form email template
+ * @returns {string} The rendered HTML of the contact form email
+ */
+export const renderContactFormHtml = (props: ContactFormEmailProps): string =>
+  pupa(contactFormHtml, props);
